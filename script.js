78a5048786859a488c05612a3951d73e5fbe84ee
Jaylen <jnixo0033@launchpadphilly.org>
2024-11-01 15:13:38 -0400

    Projects

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //////////////////////////////JavaScript For index/////////////////////////////////////////////////////
-if (window.location.href === "\index.html"){
-
+if (window.location.href === "index.html") {
+ }
+function hidePlayer() {
+    var x = document.getElementById("hideElement");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-
